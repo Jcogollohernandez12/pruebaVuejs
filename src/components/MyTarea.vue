f1354d913904ec099c22a8dee3771824618a6a6f
Jcogollo12 <josandres123@gmail.com>
2022-10-03 22:39:22 -0500

    first commit

---
+++ src/components/MyTarea.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1 class="display-4 text-center">Hola Mundo!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyTarea'
+    }
+</script>
+
+<style scoped>
+
+</style>
